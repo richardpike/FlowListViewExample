d0a06e09417d4dccc18958437c489bf91d69aaf8
Richard Pike <richard.pike@argonelectronics.com>
2017-10-18 16:27:03 +0100

    don't use the GUID - 1, 2, 3 - easier to read.

--- FLV_Example/FLV_Example/Services/MockDataStore.cs
+++ FLV_Example/FLV_Example/Services/MockDataStore.cs
@@ -15,9 +15,9 @@ namespace FLV_Example
 			items = new List<Item>();
 			var mockItems = new List<Item>
 			{
-				new Item { Id = Guid.NewGuid().ToString(), Text = "First item", Description="This is an item description." },
-				new Item { Id = Guid.NewGuid().ToString(), Text = "Second item", Description="This is an item description." },
-				new Item { Id = Guid.NewGuid().ToString(), Text = "Third item", Description="This is an item description." },
+				new Item { Id = 1, Text = "First item", Description="This is an item description." },
+				new Item { Id = 2, Text = "Second item", Description="This is an item description." },
+				new Item { Id = 3, Text = "Third item", Description="This is an item description." },
 			};
 
 			foreach (var item in mockItems)
@@ -26,34 +26,7 @@ namespace FLV_Example
 			}
 		}
 
-		public async Task<bool> AddItemAsync(Item item)
-		{
-			items.Add(item);
-
-			return await Task.FromResult(true);
-		}
-
-		public async Task<bool> UpdateItemAsync(Item item)
-		{
-			var _item = items.Where((Item arg) => arg.Id == item.Id).FirstOrDefault();
-			items.Remove(_item);
-			items.Add(item);
-
-			return await Task.FromResult(true);
-		}
-
-		public async Task<bool> DeleteItemAsync(string id)
-		{
-			var _item = items.Where((Item arg) => arg.Id == id).FirstOrDefault();
-			items.Remove(_item);
-
-			return await Task.FromResult(true);
-		}
-
-		public async Task<Item> GetItemAsync(string id)
-		{
-			return await Task.FromResult(items.FirstOrDefault(s => s.Id == id));
-		}
+		
 
 		public async Task<IEnumerable<Item>> GetItemsAsync(bool forceRefresh = false)
 		{
